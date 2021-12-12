bff17042d19a4bf204386d26ea9d98b8813abd1a
Eduardo Maxwell <eduardomaxwell2011@gmail.com>
2021-12-12 19:58:35 -0400

    added movie cover for details

--- app/src/main/java/br/com/eduardomaxwell/netflixremake/MovieActivity.java
+++ app/src/main/java/br/com/eduardomaxwell/netflixremake/MovieActivity.java
@@ -1,8 +1,12 @@
 package br.com.eduardomaxwell.netflixremake;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import br.com.eduardomaxwell.netflixremake.databinding.ActivityMovieBinding;
 
@@ -21,5 +25,13 @@ public class MovieActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back);
         }
+
+        LayerDrawable drawable = (LayerDrawable) ContextCompat.getDrawable(this, R.drawable.shadows);
+        if (drawable != null) {
+            Drawable movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4);
+            drawable.setDrawableByLayerId(R.id.cover_drawable, movieCover);
+            ((ImageView) findViewById(R.id.ivCover)).setImageDrawable(drawable);
+        }
+
     }
 }
