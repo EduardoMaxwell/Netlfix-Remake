e3d41166153cc044fe920672a9979a903b392e05
Eduardo Maxwell <eduardomaxwell2011@gmail.com>
2021-12-27 22:32:49 -0400

    improvement: created Adapter and Holder for Movies in Kotlin

--- app/src/main/java/br/com/eduardomaxwell/netflixremake/kotlin/MainActivity.kt
+++ app/src/main/java/br/com/eduardomaxwell/netflixremake/kotlin/MainActivity.kt
@@ -9,11 +9,15 @@ import androidx.recyclerview.widget.RecyclerView
 import br.com.eduardomaxwell.netflixremake.R
 import br.com.eduardomaxwell.netflixremake.databinding.ActivityMainBinding
 import br.com.eduardomaxwell.netflixremake.databinding.CategoryItemBinding
+import br.com.eduardomaxwell.netflixremake.databinding.MovieItemBinding
 import br.com.eduardomaxwell.netflixremake.model.Category
+import br.com.eduardomaxwell.netflixremake.model.Movie
 import br.com.eduardomaxwell.netflixremake.util.CategoryTask
+import br.com.eduardomaxwell.netflixremake.util.ImageDownloaderTask
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainAdapter: MainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,18 +25,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val categories = arrayListOf<Category>()
-        val mainAdapter = MainAdapter(categories)
+        mainAdapter = MainAdapter(categories)
         binding.recyclerMain.adapter = mainAdapter
         binding.recyclerMain.layoutManager = LinearLayoutManager(this)
 
         val categoryTask = CategoryTask(this)
         categoryTask.setCategoryLoader { categories1 ->
-            categories1.size
+            mainAdapter.categories.clear()
+            mainAdapter.categories.addAll(categories1)
+            mainAdapter.notifyDataSetChanged()
         }
         categoryTask.execute("https://tiagoaguiar.co/api/netflix/home")
     }
 
-    private inner class MainAdapter(val categories: List<Category>) :
+    private inner class MainAdapter(val categories: MutableList<Category>) :
         RecyclerView.Adapter<CategoryHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryHolder {
             return CategoryHolder(layoutInflater.inflate(R.layout.category_item, parent, false))
@@ -47,10 +53,39 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private class CategoryHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private inner class MovieAdapter(val movies: List<Movie>) :
+        RecyclerView.Adapter<MovieHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieHolder {
+            return MovieHolder(layoutInflater.inflate(R.layout.movie_item, parent, false))
+        }
+
+        override fun onBindViewHolder(holder: MovieHolder, position: Int) {
+            val movie = movies[position]
+            holder.bind(movie)
+        }
+
+        override fun getItemCount() = movies.size
+
+    }
+
+    private inner class CategoryHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val binding = CategoryItemBinding.bind(itemView)
         fun bind(category: Category) {
             binding.txtTitleCategory.text = category.name
+            binding.rvMovie.adapter = MovieAdapter(category.movies)
+            binding.rvMovie.layoutManager =
+                LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL, false)
+        }
+    }
+
+    private class MovieHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val binding = MovieItemBinding.bind(itemView)
+        fun bind(movie: Movie) {
+            ImageDownloaderTask(binding.ivCover)
+                .execute(movie.coverUrl)
+            binding.ivCover.setOnClickListener {
+
+            }
         }
     }
 }
